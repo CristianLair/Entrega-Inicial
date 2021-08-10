a3744c42e29cd94b3ab8e2315fee8934c8221c4f
¨CristianLair¨ <cristianlair@gmail.com>
2021-08-10 02:43:43 -0300

    puerto actualizado

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 require('./db/mongoose');
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001 ;
 const Usuario = require('./model/registro');
 const cors = require('cors');
 app.use(cors());
